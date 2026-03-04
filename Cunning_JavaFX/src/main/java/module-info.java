b34b34a3a2fd71389657c4b801f1426d0d4fd07c
AdanGavira <gaviradan@gmail.com>
2026-03-04 11:07:09 +0100

    Delete Cunning_JavaFX directory

--- Cunning_JavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.cunning_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.cunning_javafx to javafx.fxml;
-    exports org.example.cunning_javafx;
-}
